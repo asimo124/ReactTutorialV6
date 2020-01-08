700a0c3bfc5bd1c63efdd78c5e27154c73ec05da
alex hawley <alex@engage2learn.org>
2020-01-08 17:04:15 -0600

    - Working on React classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div >
+                <h1>Alex Hawley</h1>
+                <p>This is my portfolio</p>
+                <p>I enjoy working on interesting projects.</p>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Animal {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    speak() {
+        console.log('I am', this.name, 'and I am', this.age, 'years old');
+    }
+}
+
+const animal1 = new Animal('Steve', '11');
+animal1.speak();
+
+class Lion extends Animal {
+    constructor(name, age, furColor, speed) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.furColor = furColor;
+        this.speed = speed;
+    }
+
+    roar() {
+        console.log('ROOAR! My fur color is', this.furColor,
+            'and my speed is', this.speed, 'MPH');
+    }
+}
+
+const lion1 = new Lion('Tim', 8, 'tan', 50);
+lion1.speak();
+lion1.roar();
