921e7a20ed93dbb2e377b3b3f8a2586305bdcca7
alex hawley <alex@engage2learn.org>
2020-01-09 19:21:13 -0600

    - Working on Projects in portfolio, with images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+
+    state = {
+        displayBio: false
+    };
+
+    toggleDisplayBio = () => {
+        this.setState({
+            displayBio: !this.state.displayBio
+        });
+    };
+
     render() {
+
         return (
             <div >
                 <h1>Alex Hawley</h1>
                 <p>This is my portfolio</p>
                 <p>I enjoy working on interesting projects.</p>
+                {
+                    (this.state.displayBio) ? (
+                        <div >
+                            <p>I live in San Antonio</p>
+                            <p>I love front end javascript frameworks like Angular, React, and Vue.js</p>
+                            <p>I playing pool and riding scooters.</p>
+                            <button onClick={this.toggleDisplayBio}>Show Less</button>
+                        </div>
+                    ) : (
+                        <div >
+                            <button onClick={this.toggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
             </div>
         )
     }
